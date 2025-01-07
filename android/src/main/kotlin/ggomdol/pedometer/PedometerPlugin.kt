6fdaa609b94395376504a7546c6e2d1dc94bbd79
kris <kris.park@snplab.io>
2025-01-07 18:26:50 +0900

    comment

--- android/src/main/kotlin/ggomdol/pedometer/PedometerPlugin.kt
+++ android/src/main/kotlin/ggomdol/pedometer/PedometerPlugin.kt
@@ -2,21 +2,13 @@ package ggomdol.pedometer
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.util.Log
 import androidx.annotation.NonNull
-import com.google.android.gms.fitness.FitnessLocal
-import com.google.android.gms.fitness.data.LocalDataType
-import com.google.android.gms.fitness.request.LocalDataReadRequest
+import ggomdol.lib.pedometer.PedometerLib
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import java.time.Instant
-import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import java.util.concurrent.TimeUnit
 
 /** PedometerPlugin */
 class PedometerPlugin : FlutterPlugin, MethodChannel.MethodCallHandler, ActivityAware {
@@ -50,58 +42,16 @@ class PedometerPlugin : FlutterPlugin, MethodChannel.MethodCallHandler, Activity
     }
 
     private fun readStepCount() {
-        val endTime = LocalDateTime.now().atZone(ZoneId.systemDefault())
-        val startTime = endTime.minusDays(7).withHour(0).withMinute(0).withSecond(0)
-        val readRequest = LocalDataReadRequest.Builder()
-                .aggregate(LocalDataType.TYPE_STEP_COUNT_DELTA)
-                .bucketByTime(1, TimeUnit.DAYS)
-                .setTimeRange(startTime.toEpochSecond(), endTime.toEpochSecond(), TimeUnit.SECONDS)
-                .build()
-
-        activity?.let {
-            FitnessLocal.getLocalRecordingClient(it).readData(readRequest)
-                .addOnSuccessListener { response ->
-                    val resultArray = mutableMapOf<String, Int>();
-
-                    for (dataSet in response.buckets.flatMap { it.dataSets }) {
-                        for (dp in dataSet.dataPoints) {
-                            val time = Instant.ofEpochMilli(dp.getStartTime(TimeUnit.MILLISECONDS)).atZone(ZoneId.systemDefault()).toLocalDateTime()
-                            val timeText = DateTimeFormatter.ofPattern("yyyy-MM-dd").format(time)
-
-                            for (field in dp.dataType.fields) {
-                                val stepCount = dp.getValue((field)).asInt()
-                                resultArray[timeText] = stepCount
-                            }
-                        }
-                    }
-
-                    stepCountMethodCannel.invokeMethod(
-                        method_channel_pedometer_step_count,
-                        resultArray
-                    )
-                }
-                .addOnFailureListener { e ->
-                    Log.w(TAG, "There was an error reading data", e)
-                }
-        }
+        PedometerLib.readStepCountBridge(activity, object: PedometerLib.OnResult{
+            override fun onSuccess(dateAndSteps: Map<String, Int>) {
+                stepCountMethodCannel.invokeMethod(method_channel_pedometer_step_count, dateAndSteps)
+            }
+        })
     }
 
     @SuppressLint("MissingPermission")
-    private fun subscribe() {
-        activity?.let {
-            FitnessLocal.getLocalRecordingClient(it).subscribe(LocalDataType.TYPE_STEP_COUNT_DELTA)
-                .addOnSuccessListener { Log.d(TAG, "succeed subscribe") }
-                .addOnFailureListener { e -> Log.e(TAG, "failed subscribe : ${e.message}") }
-        }
-    }
-
-    private fun unSubscribe() {
-        activity?.let {
-            FitnessLocal.getLocalRecordingClient(it).unsubscribe(LocalDataType.TYPE_STEP_COUNT_DELTA)
-                .addOnSuccessListener { Log.d(TAG, "succeed unSubscribe") }
-                .addOnFailureListener { e -> Log.e(TAG, "failed unSubscribe : ${e.message}") }
-        }
-    }
+    private fun subscribe() = PedometerLib.subscribeBridge(activity)
+    private fun unSubscribe() = PedometerLib.unSubscribeBridge(activity)
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
         activity = binding.activity
